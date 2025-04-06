6c1a90c3c7b8ee2b98a19d774453a3de91bfc25b
Mark Wells <167723366+markwells13@users.noreply.github.com>
2025-04-06 16:57:16 -0400

    Update linkedlist.py

--- linkedlist.py
+++ linkedlist.py
@@ -1,43 +1,44 @@
-
 class Node:
     def __init__(self, data):
-        self.data = data      #Holds the song name and info
-        self.next = None      #Points to the next node
+        self.data = data      # Holds the song name and info
+        self.next = None      # Points to the next node
 
-# Single Linked List class
+# Singly Linked List class
 class SinglyLinkedList:
     def __init__(self):
-        self.head = None #starts the list
+        self.head = None  # Starts the list
 
     def append(self, data):
         """Add a new node to the end of the list"""
-        new_node = Node(data)   #Creates a new node with the given data
+        new_node = Node(data)  # Create a new node with the given data
 
         if not self.head:
-            #If the list is empty, make the new node the head
+            # If the list is empty, make the new node the head
             self.head = new_node
             return
 
-        #if not then traverse to the end of the list
+        # Traverse to the end of the list
         current = self.head
         while current.next:
             current = current.next
 
-        #link the current last node to the new current node
+        # Link the last node to the new node
         current.next = new_node
 
     def prepend(self, data):
-        """Add a new node at the beginning of the list"""
-        new_node = Node(data)   #Create the new node
+        #Data holds the value to be stored in the current node.
+        #Add a new node at the beginning of the list
+        new_node = Node(data)  # Create the new node
 
-        #Points the new node's next to the current head
+        # Point the new node's next to the current head
+        #Next is responsible for containing a link to the next node in the list.
         new_node.next = self.head
 
         # Update head to the new node
         self.head = new_node
 
     def display(self):
-        """Print the list from head to tail"""
+        #Print the list from head to tail
         current = self.head
 
         print("Playlist:", end=" ")
@@ -49,12 +50,12 @@ class SinglyLinkedList:
 
         print("None")
 
-#usage example:
+# Usage example:
 
-#Creates a new singly linked list for a playlist
+# Create a new singly linked list for a playlist
 playlist = SinglyLinkedList()
 
-#Apppend songs to the playlist
+# Append songs to the playlist
 playlist.append("Uptown Funk")
 playlist.append("Last Friday Night")
 playlist.append("Hot and Cold")
@@ -62,5 +63,6 @@ playlist.append("Hot and Cold")
 # Add an intro song at the beginning
 playlist.prepend("Intro")
 
-#show playlist in forward direction (from first to last song)
+# Show playlist in forward direction (from first to last song)
 playlist.display()
+
